0d783a06209ed8a594f4d70e4984a8a020ca7d99
Nicolas Trombotti <nicolastrombotti11@gmail.com>
2023-12-03 10:20:32 +0100

    terminar Veterinaria

--- veterinaria.sh
+++ veterinaria.sh
@@ -30,7 +30,7 @@ if (grep -q "$cedula_dueno" socios.txt); then
 fi
 done
 
-mascotas=""
+mascotas=" ";
 while :
 do
 echo "Ingrese la cantidad de Mascotas (hasta cuatro)."
@@ -68,6 +68,7 @@ echo "4. Salir"
 read opcion2
 case $opcion2 in
 1) echo "Agendar Cita"
+
 echo "Ingrese el nombre"
 read nombre
 echo "Ingresar el Nombre de la Mascota"
@@ -78,6 +79,10 @@ echo "Ingresar el costo"
 read costo
 echo "Ingresar la fecha"
 read fecha
+while [[ ! $fecha =~ ^[0-9]{4}-[0-1]{1}[1-9]{1}-[0-3]{1}[0-9]$ ]]; do
+    echo "Fecha invalida. Por favor, ingrese la fecha en el formato correcto (YYYY-MM-DD)"
+    read fecha
+done
 echo "Ingrese la hora"
 read hora
 
@@ -116,8 +121,43 @@ done
   fi
 ;;
 4) echo "Venta de Productos"
+   echo "Seleccione la categoría del producto: 1) Medicamentos 2) Suplementos 3) Accesorios"
+   read categoria
+   echo "Ingrese el código del producto:"
+   read codigoArticulo
+   echo "Ingrese la cantidad que desea comprar:"
+   read cantidad
+
+   if grep -q "$codigoArticulo" articulos.txt; then
+       stock=$(grep "$codigoArticulo" articulos.txt | cut -d',' -f5)
+       if (( stock >= cantidad )); then
+           echo "Compra confirmada"
+           nuevoStock=$((stock - cantidad))
+           sed -i "/$codigoArticulo/ s/\(.*,\)[0-9]*/\1$nuevoStock/" "articulos.txt"
+       else
+           echo "No hay suficientes unidades en stock. Intente con una cantidad menor."
+       fi
+   else
+       echo "El producto no existe"
+   fi
 ;;
 5) echo "Informe Mensual"
+   echo "Ingrese el mes (número entre 1 y 12):"
+   read mes
+
+   while (( mes < 1 || mes > 12 )); do
+       echo "Mes inválido. Por favor, ingrese un número entre 1 y 12:"
+       read mes
+   done
+
+   totalVentas=$(grep -c "$mes" ventas.txt)
+   if (( totalVentas > 0 )); then
+       totalRecaudado=$(awk -F',' -v mes="$mes" '$2 == mes {sum += $3} END {print sum}' ventas.txt)
+       promedio=$(echo "scale=2; $totalRecaudado / $totalVentas" | bc)
+       echo "El promedio de lo recaudado en el mes $mes es: $promedio"
+   else
+       echo "No se realizaron ventas en el mes $mes"
+   fi
 ;;
 6)exit 0;;
 #Alerta
